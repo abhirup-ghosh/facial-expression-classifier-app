cd754e9adaf1f4568a1d16315f84492846ee03c6
abhirup.ghosh <abhirup.ghosh@emmy-sharing.de>
2023-12-15 20:35:59 +0100

    1. notebook to make preprocess without TF depedency and prediction using TF-lite 2. lambda_function

---
+++ scripts/lambda_function.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import tflite_runtime.interpreter as tflite
+from PIL import Image
+from io import BytesIO
+from urllib import request
+import numpy as np
+
+
+interpreter = tflite.Interpreter(model_path='../models/emotion_classifier.tflite')
+interpreter.allocate_tensors()
+
+input_index = interpreter.get_input_details()[0]['index']
+output_index = interpreter.get_output_details()[0]['index']
+
+class_labels = ['Anger', 'Disgust', 'Fear', 'Happy', 'Neutral', 'Sadness', "Surprise"]
+
+
+def prepare_input(url):
+    
+    with request.urlopen(url) as resp:
+        buffer = resp.read()
+    stream = BytesIO(buffer)
+    img = Image.open(stream)
+    img = img.resize((48, 48), Image.NEAREST)
+
+    x = np.array(img)
+    if x.ndim > 2:
+        x = x[:,:,0]
+    else: return x
+    X = np.array([x])
+    X = X/255.
+    X = np.expand_dims(np.float32(X), axis=-1)
+
+    return X
+
+def predict(url):
+    X = prepare_input(url)
+
+    interpreter.set_tensor(input_index, X)
+    interpreter.invoke()
+    preds = interpreter.get_tensor(output_index)
+
+    float_predictions = preds[0].tolist()
+
+    return dict(zip(class_labels, float_predictions))
+
+
+def lambda_handler(event, context):
+    url = event['url']
+    result = predict(url)
+    return result
