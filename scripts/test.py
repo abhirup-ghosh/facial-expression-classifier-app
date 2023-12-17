54d42a812393581ea8a8cc14a09f2dd5945e750c
abhirup.ghosh <abhirup.ghosh@emmy-sharing.de>
2023-12-17 20:54:50 +0100

    test file added to test lambda function

--- scripts/test.py
+++ scripts/test.py
@@ -2,7 +2,7 @@ import requests
 
 url = 'http://localhost:8080/2015-03-31/functions/function/invocations'
 
-data = {'url': 'https://i.pinimg.com/originals/6e/cd/8a/6ecd8a85b2459b07d2889667aa9b6a0c.jpg'}
+data = {'url': 'https://upload.wikimedia.org/wikipedia/commons/0/09/The_joy_of_the_happy_face_by_Rasheedhrasheed.jpg'}
 
 result = requests.post(url, json=data).json()
 print(result)
