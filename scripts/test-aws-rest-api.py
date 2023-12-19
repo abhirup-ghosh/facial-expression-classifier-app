9e390f5edb3150f94c0eba1b1876dd5d61938332
abhirup.ghosh <abhirup.ghosh@emmy-sharing.de>
2023-12-19 19:06:33 +0100

    updated test script

--- scripts/test-aws-rest-api.py
+++ scripts/test-aws-rest-api.py
@@ -2,8 +2,7 @@ import requests
 
 url = 'https://xowsdry1bc.execute-api.eu-north-1.amazonaws.com/Test/predict'
 
-data = {'url': 'https://assets.telegraphindia.com/abp/2018/6/24/1606282415_5fbdecaf96376_abhirup-ghosh.jpg'}
-#'https://upload.wikimedia.org/wikipedia/commons/0/09/The_joy_of_the_happy_face_by_Rasheedhrasheed.jpg'}
+data = {'url': 'https://upload.wikimedia.org/wikipedia/commons/0/09/The_joy_of_the_happy_face_by_Rasheedhrasheed.jpg'}
 
 result = requests.post(url, json=data).json()
 print(result)
